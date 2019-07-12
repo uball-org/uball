d2920ad5111a3c9c966448ecad52c9ac47757df1
Ali Baraka <baraka.ali210@gmail.com>
2019-07-12 10:16:59 -0500

    adding app and example properties folder

--- src/test/java/com/uball/uballapp/UballAppApplicationTests.java
+++ src/test/java/com/uball/uballapp/UballAppApplicationTests.java
@@ -1,16 +1,16 @@
-package com.uball.uballapp;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class UballAppApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.uball.uballapp;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class UballAppApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
