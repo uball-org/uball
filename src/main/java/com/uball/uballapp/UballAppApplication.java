6c3a72a4b92ba37286ecaf8dddff30afa16c0568
Ali Baraka <baraka.ali210@gmail.com>
2019-07-12 10:03:23 -0500

    initial commit

---
+++ src/main/java/com/uball/uballapp/UballAppApplication.java
@@ -0,0 +1,13 @@
+package com.uball.uballapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UballAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UballAppApplication.class, args);
+	}
+
+}
