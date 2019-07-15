ad6d4746a97ddea6548df30d44a88b366f3ac871
Mykal Clinard <mykalclinard@gmail.com>
2019-07-15 17:02:02 -0500

    repos in progress

--- src/main/java/com/uball/uballapp/repos/UserRepository.java
+++ src/main/java/com/uball/uballapp/repos/UserRepository.java
@@ -13,20 +13,25 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository <User, Long> {
-    @Query("select id, concat(first_name, ' ', last_name) as Name," +
-            "gender as Gender, points as Points from users" +
-            "where league_id in ( select id from leagues where " +
-            "id = 1)")
-    User findByLeague(String email);
+    @Query("select id, concat(first_name, ' ', last_name) as Name, " +
+            "       gender as Gender," +
+            "       points as Total_Points " +
+            "from users " +
+            "where league_id in (" +
+            "    select id" +
+            "    from leagues" +
+            "    where id = ?1" +
+            "    )")
+    User findByLeague(long id);
 
-    @Query("select name from machines\n" +
-            "where id in (\n" +
-            "    select machine_id\n" +
-            "    from scores\n" +
-            "    where user_id in (\n" +
-            "        select id\n" +
-            "        from users\n" +
-            "        where id = 3\n" +
+    @Query("select name from machines " +
+            "where id in (" +
+            "    select machine_id " +
+            "    from scores " +
+            "    where user_id in (" +
+            "        select id " +
+            "        from users " +
+            "        where id = 3 " +
             "    )" +
             ")")
     List<Machine> searchByMachine(@Param("id") long id);
@@ -36,5 +41,14 @@ public interface UserRepository extends CrudRepository <User, Long> {
 
     @Query("select u from #{#entityName} u where u.email = ?1")
     List<User> findByEmail(String email);
+
+
+    @Query("select u.id, concat(u.first_name, ' ', u.last_name) as Name," +
+            "u.gender as Gender, s.score as Score " +
+            "from users u " +
+            "join scores s on u.id = s.user_id " +
+            "order by s.score desc, " +
+            "limit, 4")
+    List<User> findTop4();
 
 }
