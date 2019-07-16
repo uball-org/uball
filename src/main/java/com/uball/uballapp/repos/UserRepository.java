933b009dc7424c20bc986977bbf960e7d5f4fd3b
Mykal Clinard <mykalclinard@gmail.com>
2019-07-16 05:05:14 -0500

    repos in progress 16

--- src/main/java/com/uball/uballapp/repos/UserRepository.java
+++ src/main/java/com/uball/uballapp/repos/UserRepository.java
@@ -13,6 +13,24 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository <User, Long> {
+
+    //------------------------------
+
+    //    Two forms of "findByEmail"
+
+    User findByEmail(String email);
+
+//    @Query("select u from #{#entityName} u where u.email = ?1")
+//    User findByEmail(String email);
+    //------------------------------
+
+//    Get user by their id
+
+    @Query("from User u where u.id = ?1")
+    User getUserById(long id);
+
+//    Find all who are in one league by league id
+
     @Query("select id, concat(first_name, ' ', last_name) as Name, " +
             "       gender as Gender," +
             "       points as Total_Points " +
@@ -24,6 +42,8 @@ public interface UserRepository extends CrudRepository <User, Long> {
             "    )")
     User findByLeague(long id);
 
+//  get the machines a user has played on (SUBQuery)
+
     @Query("select name from machines " +
             "where id in (" +
             "    select machine_id " +
@@ -31,17 +51,13 @@ public interface UserRepository extends CrudRepository <User, Long> {
             "    where user_id in (" +
             "        select id " +
             "        from users " +
-            "        where id = 3 " +
+            "        where id = ?1 " +
             "    )" +
             ")")
     List<Machine> searchByMachine(@Param("id") long id);
 
-    @Query("from User u where u.id = ?1")
-    User getUserById(long id);
 
-    @Query("select u from #{#entityName} u where u.email = ?1")
-    List<User> findByEmail(String email);
-
+//  Top 4 of all time
 
     @Query("select u.id, concat(u.first_name, ' ', u.last_name) as Name," +
             "u.gender as Gender, s.score as Score " +
@@ -50,5 +66,13 @@ public interface UserRepository extends CrudRepository <User, Long> {
             "order by s.score desc, " +
             "limit, 4")
     List<User> findTop4();
+
+//    Find top scores for machines user has played on (JOIN)
+    @Query("")
+    List<Machine> usersMachineScore(@Param("id") long id);
+
+
+//    Example of adding users to joining users_groups table
+//    @Query("insert into users_groups (user_id, group_id) VALUES (?1, ?2)")
 
 }
