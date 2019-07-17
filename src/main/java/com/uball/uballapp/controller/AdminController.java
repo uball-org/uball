216f7f529de82bc65bbd2d4a55e5255b92b7f7fc
Mykal Clinard <mykalclinard@gmail.com>
2019-07-17 16:09:01 -0500

    admin page button not mapping

--- src/main/java/com/uball/uballapp/controller/AdminController.java
+++ src/main/java/com/uball/uballapp/controller/AdminController.java
@@ -1,38 +1,63 @@
 package com.uball.uballapp.controller;
 
 
+import com.uball.uballapp.models.Group;
+import com.uball.uballapp.repos.GroupRepository;
 import com.uball.uballapp.repos.MachineRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import com.uball.uballapp.models.User;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
 import com.uball.uballapp.repos.AdminRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class AdminController {
     private AdminRepository adminDao;
     private MachineRepository machineDao;
+    private GroupRepository groupDao;
 
-    public AdminController(AdminRepository adminDao, MachineRepository machineDao) {
+    public AdminController(AdminRepository adminDao, MachineRepository machineDao, GroupRepository groupDao) {
         this.adminDao = adminDao;
         this.machineDao = machineDao;
+        this.groupDao = groupDao;
     }
 
     //Getting all Users!
-    @GetMapping("/admindashboard") // needs to be factored to use the "id of button" for this element
+    @GetMapping("/admindashboard")
     public String all(Model model){
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 //        model.addAttribute("user", adminDao.findOne(id));
         model.addAttribute("users", adminDao.findAll());
         model.addAttribute("machines", machineDao.findAll());
         return "admin/admindashboard";
     }
 
-//
+    //Getting new Group!
+    @GetMapping("/admindashboard/creategroup")
+    public String newUserGroupList(Model model){
+
+        return "admin/admindashboard";
+    }
+
+    @PostMapping("admin/admindashboard")
+    public String createPost(){
+
+        return "redirect:/admindashboard";
+    }
+
+
+    //Getting new Group!
+    @GetMapping("/admindashboard/createmachine")
+    public String newMachineList(Model model){
+
+        return "admin/admindashboard";
+    }
+
+
+    // disable button
 //
 //    @PostMapping("/user/{id}/disable") //This will actually be to disable NOT DELETE
 //    public String delete(@PathVariable long id) {
