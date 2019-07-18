7bb38e07977cfd0c3845980da5badad7017fc1c9
Nadia Botello <botello.nadia.g@gmail.com>
2019-07-18 10:19:51 -0500

    navbar fully functional

--- src/main/java/com/uball/uballapp/configure/SecurityConfiguration.java
+++ src/main/java/com/uball/uballapp/configure/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
 //                **********
-                .logoutSuccessUrl("/login?logout") // append a query string value
+                .logoutSuccessUrl("/") // append a query string value
 //                ***********
 
                 /* Pages that can be viewed without having to log in */
