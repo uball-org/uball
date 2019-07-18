5883d9413d1d87186078889090cce483f046fefa
Mykal Clinard <mykalclinard@gmail.com>
2019-07-18 15:06:43 -0500

    lists into groups

--- src/main/java/com/uball/uballapp/controller/AdminController.java
+++ src/main/java/com/uball/uballapp/controller/AdminController.java
@@ -2,8 +2,10 @@ package com.uball.uballapp.controller;
 
 
 import com.uball.uballapp.models.Group;
+import com.uball.uballapp.models.Machine;
 import com.uball.uballapp.repos.GroupRepository;
 import com.uball.uballapp.repos.MachineRepository;
+import com.uball.uballapp.repos.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import com.uball.uballapp.models.User;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +19,19 @@ import java.util.List;
 @Controller
 public class AdminController {
     private AdminRepository adminDao;
+    private UserRepository userDao;
     private MachineRepository machineDao;
     private GroupRepository groupDao;
 
-    public AdminController(AdminRepository adminDao, MachineRepository machineDao, GroupRepository groupDao) {
+    public AdminController(AdminRepository adminDao,
+                           MachineRepository machineDao,
+                           GroupRepository groupDao,
+                           UserRepository userDao) {
+
         this.adminDao = adminDao;
         this.machineDao = machineDao;
         this.groupDao = groupDao;
+        this.userDao = userDao;
     }
 
     //Getting all Users!
@@ -35,27 +43,32 @@ public class AdminController {
         return "admin/admindashboard";
     }
 
-    //Getting new Group!
-    @GetMapping("/admindashboard/creategroup")
-    public String newUserGroupList(Model model){
+//    Value of user selected on view is : "uchecked"
 
+    @RequestMapping(value = "/admindashboard/creategroup", method = RequestMethod.POST)
+    public String newUsersForGroups(Model model, @RequestParam(name = "uchecked") List<User> newU) {
+        System.out.println(newU.size());
+        System.out.println(newU.get(0).getFirstName());
+        for(User users : newU){
+            users.getGroups().add(groupDao.findOne(1L));
+            userDao.save(users);
+            System.out.println(groupDao.findOne(1L).getUsers().size());
+        }
         return "admin/admindashboard";
     }
 
-    @PostMapping("admin/admindashboard")
-    public String createPost(){
 
-        return "redirect:/admindashboard";
+    //    Value of user selected on view is : "mchecked"
+
+    @RequestMapping(value = "/admindashboard/createmachines", method = RequestMethod.POST)
+    public String newMachines(Model model, @RequestParam(name = "mchecked") List<Machine> newM) {
+        System.out.println(newM.size()/2);
+        System.out.println(newM.get(0).getName());
+//        for(Machine machine : newM){
+//            machineDao.save(machine);
+//        }
+        return "redirect: /admindashboard";
     }
-
-
-    //Getting new Group!
-    @GetMapping("/admindashboard/createmachine")
-    public String newMachineList(Model model){
-
-        return "admin/admindashboard";
-    }
-
 
     // disable button
 //
