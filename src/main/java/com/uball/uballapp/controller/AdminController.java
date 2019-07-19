0bfc526bcf20dac807613993a4a34d6238221f09
Mykal Clinard <mykalclinard@gmail.com>
2019-07-19 13:38:24 -0500

    Will take in new list of users and machines, then create blank scores in score table, and assign the users to group that is hard coded and send to the user_group table

--- src/main/java/com/uball/uballapp/controller/AdminController.java
+++ src/main/java/com/uball/uballapp/controller/AdminController.java
@@ -3,76 +3,86 @@ package com.uball.uballapp.controller;
 
 import com.uball.uballapp.models.Group;
 import com.uball.uballapp.models.Machine;
-import com.uball.uballapp.repos.GroupRepository;
-import com.uball.uballapp.repos.MachineRepository;
-import com.uball.uballapp.repos.UserRepository;
+import com.uball.uballapp.models.Score;
+import com.uball.uballapp.repos.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import com.uball.uballapp.models.User;
 import org.springframework.web.bind.annotation.*;
-import com.uball.uballapp.repos.AdminRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 public class AdminController {
     private AdminRepository adminDao;
     private UserRepository userDao;
+    private ScoreRepository scoreDao;
     private MachineRepository machineDao;
     private GroupRepository groupDao;
 
     public AdminController(AdminRepository adminDao,
                            MachineRepository machineDao,
                            GroupRepository groupDao,
-                           UserRepository userDao) {
+                           UserRepository userDao,
+                           ScoreRepository scoreDao) {
 
         this.adminDao = adminDao;
         this.machineDao = machineDao;
         this.groupDao = groupDao;
         this.userDao = userDao;
+        this.scoreDao = scoreDao;
     }
-
-    //Getting all Users!
+            //All Machines and Users
     @GetMapping("/admindashboard")
     public String all(Model model){
-//        model.addAttribute("user", adminDao.findOne(id));
         model.addAttribute("users", adminDao.findAll());
         model.addAttribute("machines", machineDao.findAll());
+
         return "admin/admindashboard";
     }
 
-//    Value of user selected on view is : "uchecked"
-
-    @RequestMapping(value = "/admindashboard/creategroup", method = RequestMethod.POST)
-    public String newUsersForGroups(Model model, @RequestParam(name = "uchecked") List<User> newU) {
-        System.out.println(newU.size());
-        System.out.println(newU.get(0).getFirstName());
+            //Value of user selected on view is : "uchecked" / "mchecked"
+    @RequestMapping(value = "/admindashboard/creategrouping", method = RequestMethod.POST)
+    public String newUsersForGroups(Model model,
+                                    @RequestParam(name = "uchecked") List<User> newU,
+                                    @RequestParam(name = "mchecked") List<Machine> newM) {
         for(User users : newU){
-            model.addAttribute("usergroup", adminDao.findAll());
-            users.getGroups().add(groupDao.findOne(1L));
+            System.out.println("users = " +
+                    newU.indexOf(users) + " " +
+                    users.isAdmin() + " " +
+                    users.getGroups() + " " +
+                    users.getFirstName() + " " +
+                    users.getLastName() + " " + users.getId());
+
+            //Create logic for making group amount and number of groups
+
+            for(Machine machine : newM){
+                System.out.println(machine.getId() + " " +
+                        machine.getName() + " " +
+                        machine.getScores());
+
+                Score blankscore = new Score();
+                blankscore.setScore(0);
+                blankscore.setMachine(machine);
+                blankscore.setUser(users);
+                scoreDao.save(blankscore);
+//                model.addAttribute("matchmachines", blankscore);
+
+            }
+            users.getGroups().add(groupDao.findOne(2L));
             userDao.save(users);
-            System.out.println(groupDao.findOne(1L).getUsers().size());
         }
 
-        return "admin/admindashboard";
+        System.out.println("size of new users list divide by two= " + newU.size()/2);
+        System.out.println("size of users in group 1 = " + groupDao.findOne(1L).getUsers().size());
+        System.out.println("size of new machines list divide by two= " + newM.size()/2);
+
+        return "redirect:/admindashboard";
     }
 
-
-    //    Value of user selected on view is : "mchecked"
-
-    @RequestMapping(value = "/admindashboard/createmachines", method = RequestMethod.POST)
-    public String newMachines(Model model, @RequestParam(name = "mchecked") List<Machine> newM) {
-        System.out.println(newM.size()/2);
-        System.out.println(newM.get(0).getName());
-        model.addAttribute(newM);
-        for(Machine machine : newM){
-            System.out.println(newM.get(newM.indexOf(machine)).getName());
-        }
-        return "admin/admindashboard";
-//            machineDao.save(machine);
-    }
 
     // disable button
 //
