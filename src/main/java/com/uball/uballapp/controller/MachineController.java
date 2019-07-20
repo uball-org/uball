035f79103321374538b8c37376fab0bbd1ef0f76
Ali Baraka <baraka.ali210@gmail.com>
2019-07-19 22:39:21 -0500

    controler: when ' /machines' URI is accessed, displays machines index page

---
+++ src/main/java/com/uball/uballapp/controller/MachineController.java
@@ -0,0 +1,38 @@
+package com.uball.uballapp.controller;
+
+import com.uball.uballapp.models.Machine;
+import com.uball.uballapp.repos.MachineRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MachineController {
+    private MachineRepository machineDao;
+
+
+    public MachineController(MachineRepository machineDao) {
+        this.machineDao = machineDao;
+    }
+
+
+    /**
+     * ounces URI accessed, displays machines page
+     * */
+    @GetMapping("/machines")
+    public String index(Model model) {
+        model.addAttribute("machines", machineDao.findAll());
+        return "machines/index";
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
