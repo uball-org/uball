d981443b9c007dfd995e90e9d2230bc6d411d481
Mykal Clinard <mykalclinard@gmail.com>
2019-07-24 00:14:28 -0500

    Pages locked down for admin use only for CRUD

--- src/main/java/com/uball/uballapp/controller/UserController.java
+++ src/main/java/com/uball/uballapp/controller/UserController.java
@@ -76,8 +76,8 @@ public class UserController<leagueRepository> {
     public String all(Model model){
         model.addAttribute("users",userDao.findTop4ScoringUsers());
         //data by league
-//        model.addAttribute("oneLeagueScores",scoreDao.Top4ScoresByLeague(1));
-//        model.addAttribute("twoLeagueScores",scoreDao.Top4ScoresByLeague(2));
+        model.addAttribute("oneLeagueScores",scoreDao.Top4ScoresByLeague(1));
+        model.addAttribute("twoLeagueScores",scoreDao.Top4ScoresByLeague(2));
 
         return "league/leaguedashboard";
     }
