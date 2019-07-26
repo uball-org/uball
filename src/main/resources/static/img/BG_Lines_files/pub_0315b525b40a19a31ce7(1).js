1601c8c172fb33bf81da68a6f3d66ba2c8258e42
Ali Baraka <baraka.ali210@gmail.com>
2019-07-26 15:38:45 -0500

    styled league page with paarallax

---
+++ src/main/resources/static/img/BG_Lines_files/pub_0315b525b40a19a31ce7(1).js
@@ -0,0 +1,5 @@
+function rfsnLoadScript(e,t,a){var n=document.createElement("script");return n.type="text/javascript",n.async=0,n.defer=0,n.readyState?n.onreadystatechange=function(){("loaded"==n.readyState||"complete"==n.readyState)&&(n.onreadystatechange=null,t())}:n.onload=function(){"undefined"!=typeof t&&t()},n.src=e,a?(o=document.getElementsByTagName("script")[0],o.parentNode.insertBefore(n,o)):document.getElementsByTagName("head")[0].appendChild(n),n};
+
+
+var _refersion=function(a){"undefined"==typeof a&&(a=function(){});rfsnLoadScript("https://webflow.refersion.com/tracker/v3/merchant/pub_0315b525b40a19a31ce7.js?v="+Math.floor(100*Math.random()),a)};
+
